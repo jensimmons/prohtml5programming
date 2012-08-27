6ce41ed0a4fe4f058f533c20f50499c28ae47e82
Jen Simmons <jen@jensimmons.com>
2012-08-27 17:50:59 -0400

    a bunch of styling for chapter 2 in v3

--- v3/config.rb
+++ v3/config.rb
@@ -1,13 +1,24 @@
-require 'seasons'
 # Require any additional compass plugins here.
+require 'seasons'
 
 
-# Set this to the root of your project when deployed:
-http_path = "/"
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :development
+#environment = :production
+
+# In development, we can turn on the FireSass-compatible debug_info.
+#firesass = false
+firesass = true
+
+
+# Location of the projects's resources.
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+# Set this to the root of your project when deployed:
+http_path = "/"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -19,8 +30,25 @@ javascripts_dir = "javascripts"
 # line_comments = false
 
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+##
+## You probably don't need to edit anything below this.
+##
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from server root.
+relative_assets = true
+
+# Pass options to sass.
+# - For development, we turn on the FireSass-compatible debug_info.
+# - For production, we force the CSS to be regenerated even though the source
+#   scss may not have changed, since we want the CSS to be compressed and have
+#   the debug info removed.
+sass_options = (environment == :development) ? {:debug_info => true} : {:always_update => true}
+
+
