7fa979753de005d6854387cb00bf69b5be3b6784
Chris O'Connor <chriso1515@gmail.com>
2012-12-12 16:55:35 -0500

    dynamic toc

--- v4/js/navigation.js
+++ v4/js/navigation.js
@@ -1,27 +1,36 @@
 var menuObj = [];
 jQuery(function () {
+  // toc
+  if(toc) {
+    var tocOutput = '<div class="btn-group toc-link">\n' +
+    '  <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">\n' +
+    '    Table of Contents\n' +
+    '    <span class="caret"></span>\n' +
+    '  </a>\n' +
+    '<ul class="dropdown-menu">\n';
+    
+    for(var i=0, tocL = toc.length; i < tocL; i++) {
+      
+      for(var key in toc[i]) {
+        var currentClass = '';
+        if(toc[i].hasOwnProperty(key)) {
+          // if (key == currentChap) {
+          //   currentClass = 'link-current';
+          // }
+          tocOutput += ('<li class="chap"><a href="' + toc[i][key] + '" class="' + currentClass + '">' + key + '</a></li>\n');
+        }
+      }
+    }
+    
+    tocOutput += '</ul>\n</div>\n';
+    jQuery('.menu-trigger').after(jQuery(tocOutput));
+  }  
+  
+  
   var h1 = $('h1').eq(0);
   var currentChap = h1.text();
         
   var output = '<a name="main-navigation" id="main-navigation"></a><div class="nav-container"><nav role="navigation" id="menu"><ul>';
-  // if(toc) {
-  //   output += ('<li class="toc"><a href="#" class="link-toc">Table of Contents</a>\n');
-  //   output += '<ul>';
-  //   for(var i=0; i < toc.length; i++) {
-  //     
-  //     for(var key in toc[i]) {
-  //       var currentClass = '';
-  //       if(toc[i].hasOwnProperty(key)) {
-  //         if (key == currentChap) {
-  //           currentClass = 'link-current';
-  //         }
-  //         output += ('<li class="chap"><a href="' + toc[i][key] + '" class="' + currentClass + '">' + key + '</a></li>\n');
-  //       }
-  //     }
-  //     
-  //   }
-  //   output += '</ul></li>';
-  // }
     
   jQuery('<a name="item_0" />').insertBefore(h1);
   output += ('<li class="current"><a href="#item_0" >' + currentChap + '</a>\n');
