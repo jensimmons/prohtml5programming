4ae34ae2e3a54442b51816112f22c23c4876516b
Chris O'Connor <chriso1515@gmail.com>
2012-12-30 13:26:48 -0500

    chap11 part 1

--- v4/js/toc.js
+++ v4/js/toc.js
@@ -3,4 +3,5 @@ var toc = [
   {'Chapter 2: Using the Canvas API': 'ch2.html'},
   {'Chapter 3: Working with Scalable Vector Graphics': 'ch3.html'},
   {'Chapter 4: Working with Audio and Video': 'ch4.html'},
+  {'Chapter 11: Using the Web Storage API': 'ch11.html'}
 ];
