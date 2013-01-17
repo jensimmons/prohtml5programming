ea0fcb8e647b90fc697a0deba99a34690ce881cc
Jen Simmons <jen@jensimmons.com>
2013-01-17 18:18:56 -0500

    moved around front matter content, cleaned up a couple other thigns

--- v4/js/toc.js
+++ v4/js/toc.js
@@ -1,5 +1,6 @@
 var toc = [
-  {'Video Intro to This Book': 'ch0.html'},
+  {'Cover': 'ch-cover.html'},
+  {'Introduction': 'ch0.html'},
   {'Chapter 1: Overview of HTML5': 'ch1.html'},
   {'Chapter 2: Using the Canvas API': 'ch2.html'},
   {'Chapter 3: Working with Scalable Vector Graphics': 'ch3.html'},
@@ -12,5 +13,6 @@ var toc = [
   {'Chapter 10: Using the Web Workers API': 'ch10.html'},
   {'Chapter 11: Using the Web Storage API': 'ch11.html'},
   {'Chapter 12: Creating HTML5 Offline Web Applications': 'ch12.html'},
-  {'Chapter 13: The Future of HTML5': 'ch13.html'}
+  {'Chapter 13: The Future of HTML5': 'ch13.html'},
+  {'Example Video for Testing': 'ch-test.html'}
 ];
