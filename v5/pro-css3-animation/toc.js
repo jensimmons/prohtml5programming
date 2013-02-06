8b63d6a8f1513165203d85ad8f1b30f1e4955371
Chris O'Connor <chriso1515@gmail.com>
2013-02-06 17:24:39 -0500

    roc for unavailable chapters

--- v5/pro-css3-animation/toc.js
+++ v5/pro-css3-animation/toc.js
@@ -1,15 +1,15 @@
 var toc = [
   {'Cover': 'ch-cover.html'},
-  {'Introduction': 'ch2.html'},
-  {'Chapter 1: CSS3 Fundamentals': 'ch2.html'},
+  {'Introduction': ''},
+  {'Chapter 1: CSS3 Fundamentals': ''},
   {'Chapter 2: CSS3 Transforms & Transitions': 'ch2.html'},
-  {'Chapter 3: Transition Effects for Images': 'ch2.html'},
-  {'Chapter 4: Transition Effects for UI Elements': 'ch2.html'},
-  {'Chapter 5: Introduction to Keyframe Animations': 'ch2.html'},
-  {'Chapter 6: Keyframe Animations for Web Content': 'ch2.html'},
-  {'Chapter 7: Integrating CSS3 Animations with SVG & Filters': 'ch2.html'},
-  {'Chapter 8: Animated Responsive Web Design': 'ch2.html'},
-  {'Chapter 9: CSS 3D & Parallax Effects': 'ch2.html'},
-  {'Chapter 10: Tools & Technologies': 'ch2.html'}
+  {'Chapter 3: Transition Effects for Images': ''},
+  {'Chapter 4: Transition Effects for UI Elements': ''},
+  {'Chapter 5: Introduction to Keyframe Animations': ''},
+  {'Chapter 6: Keyframe Animations for Web Content': ''},
+  {'Chapter 7: Integrating CSS3 Animations with SVG & Filters': ''},
+  {'Chapter 8: Animated Responsive Web Design': ''},
+  {'Chapter 9: CSS 3D & Parallax Effects': ''},
+  {'Chapter 10: Tools & Technologies': ''}
 ];
 
