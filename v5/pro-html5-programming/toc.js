3687b75276fc32463bad041e335ed7ca2bb5d7ad
Jen Simmons <jen@jensimmons.com>
2013-02-22 14:42:31 -0500

    Rearranging the order of front matter content. And moving Intro Video.  And where the cover images link to.

--- v5/pro-html5-programming/toc.js
+++ v5/pro-html5-programming/toc.js
@@ -5,8 +5,9 @@ APRI.toc = {
     'dek':'by Peter Lubbers, Brian Albers, and Frank Salim'
   },
   'chapters': [
-    {'Cover': 'ch-cover.html'},
-    {'Introduction': 'ch0.html#introduction'},
+    {'Cover': 'cover.html'},
+    {'Introduction': 'introduction.html'},
+    {'Video Introduction': 'ch0.html'},
     {'Chapter 1: Overview of HTML5': 'ch1.html'},
     {'Chapter 2: Using the Canvas API': 'ch2.html'},
     {'Chapter 3: Working with Scalable Vector Graphics': 'ch3.html'},
@@ -20,6 +21,6 @@ APRI.toc = {
     {'Chapter 11: Using the Web Storage API': 'ch11.html'},
     {'Chapter 12: Creating HTML5 Offline Web Applications': 'ch12.html'},
     {'Chapter 13: The Future of HTML5': 'ch13.html'},
-    {'Example Video for Testing': 'ch-test.html'}
+    {'Book Information': 'info.html'}
   ]
 };
